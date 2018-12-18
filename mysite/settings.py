ac38f6722f5de6697004f2e7f4073b1f6d12c0e6
ByounghoPark <bhpark70@gmail.com>
2018-12-18 16:45:43 +0900

    improved blog, secure web, reply

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,3 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# redirect page after login and logout
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
